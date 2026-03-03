10c0e3f052ea491145f63e429a847b19652c30a1
Till Krahmer <kradu526@gmail.com>
2026-03-03 20:17:45 +0100

    fixed narroband header file

--- main/narrowband.h
+++ main/narrowband.h
@@ -6,11 +6,9 @@ extern "C" {
 #include <sys/types.h>
 
 void init_narrowband(void);
-void enqueue_command(const uint8_t* data, size_t len);
 // Dequeue into caller-provided buffer; returns number of bytes written,
 // 0 if no data, -1 on error or if buffer is too small
 ssize_t dequeue_command(uint8_t* buf, size_t max_len);
-void handle_receive(void);
 void narrowband_thread(void);
 void destroy_narrowband(void);
 
